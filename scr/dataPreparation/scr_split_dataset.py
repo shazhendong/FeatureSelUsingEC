8af7d2a649b67bcc9ad38729da046bb7f09c57fe
Zhendong Sha <17443322+shazhendong@users.noreply.github.com>
2022-11-21 12:13:48 -0500

    Add data preparation procedure

---
+++ scr/dataPreparation/scr_split_dataset.py
@@ -0,0 +1,24 @@
+# This scr split dataset into train and test datasets
+# Parameters:
+#   1: input dataset
+#   2: output train dataset
+#   3: output test dataset
+#   4: train dataset pct
+#   5: random seed
+
+import sys
+from sklearn.model_selection import train_test_split
+
+if __name__ == "__main__":
+    # Read input dataset
+
+    # Split dataset into train and test datasets
+
+    # Write train and test datasets
+    
+    
+
+    
+    
+
+
