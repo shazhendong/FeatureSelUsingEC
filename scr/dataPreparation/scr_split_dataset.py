3e9c31987d084e7fd7ff8b6329396f9147aff55d
18lz <18lz@queensu.ca>
2022-12-14 03:37:21 +0800

    Li Zhu update
    data preparation

--- scr/dataPreparation/scr_split_dataset.py
+++ scr/dataPreparation/scr_split_dataset.py
@@ -1,24 +1,32 @@
-# This scr split dataset into train and test datasets
-# Parameters:
-#   1: input dataset
-#   2: output train dataset
-#   3: output test dataset
-#   4: train dataset pct
-#   5: random seed
-
-import sys
+import os
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
-if __name__ == "__main__":
-    # Read input dataset
+def attribute_tran(y):
+    if isinstance(y[0], str):
+        for index in range(len(y)):
+            if y[index] == 'normal':
+                y[index] = 0
+            else:
+                y[index] = 1
+    y = pd.Series(y, name='Labels')
+    return y
 
-    # Split dataset into train and test datasets
+def train_test(file, x, y, train_pct, seed):
+    train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=1-train_pct, random_state=seed, stratify=y)
+    print(file + ': succeed')
+    train = pd.concat([train_x, train_y], axis=1)
+    test = pd.concat([test_x, test_y], axis=1)
+    return train, test
 
-    # Write train and test datasets
-    
-    
+def save_data(train,test,file,train_path):
+    save_path = train_path + '/' + file + '/'
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
 
-    
-    
+    train.to_csv(save_path + '/' + file + '_train.csv', index=False)
+    test.to_csv(save_path + '/' + file + '_test.csv', index=False)
+
+
 
 
